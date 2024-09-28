53c9f7360f4d2a73cc2f818fd3642e54180e811c
sigmasternchen <git@sigma-star.io>
2024-09-28 20:38:31 +0200

    feat: Add support for keyboard input

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {CellState} from "../model/CellState";
 import {Key} from "./Key";
+import {id} from "../utils";
 
 export const Keyboard = ({enabled, used, onKey}) => {
     const keys = [
@@ -8,6 +9,21 @@ export const Keyboard = ({enabled, used, onKey}) => {
         "A", "S", "D", "F", "G", "H", "J", "K", "L", null,
         "ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACK"
     ];
+
+    useEffect(() => {
+        const keyDownHandler = event => {
+            if (event.key === "Enter") {
+                onKey("ENTER");
+            } else if (event.key === "Backspace") {
+                onKey("BACK");
+            } else if (event.key.length === 1 && keys.filter(id).indexOf(event.key.toUpperCase())) {
+                onKey(event.key.toUpperCase());
+            }
+        };
+
+        window.addEventListener("keydown", keyDownHandler);
+        return () => window.removeEventListener("keydown", keyDownHandler);
+    }, [onKey]);
 
     let newlineCounter = 0;
     return <div className={"keyboard " + (!enabled ? "disabled" : "")}>
